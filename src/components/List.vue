3cc77e94bd48076435b0fef540e82a18de28ce97
CHUNDOOIN <chun.doo.in@gmail.com>
2022-08-05 14:33:18 +0900

    220805 vue강의

--- src/components/List.vue
+++ src/components/List.vue
@@ -4,21 +4,32 @@
             <h4>{{ myBlogList[0].title }}</h4>
             <p>{{ myBlogList[0].content }}</p>
             <p>{{ myBlogList[0].date }}</p>
-            <router-link to="/detail">디테일 페이지로</router-link>
+
+            <!-- 강의에 나온 방법 -->
+            <!-- <router-link to="/detail">디테일 페이지로</router-link> -->
+
+            <!-- 디테일 페이지의 각각의 주소로 이동하는 방법 -->
+            <router-link :to="{ path: 'detail/' + myBlogList[0].number }"
+                >디테일 페이지로</router-link
+            >
         </div>
 
         <div class="blog-detail">
             <h4>{{ myBlogList[1].title }}</h4>
             <p>{{ myBlogList[1].content }}</p>
             <p>{{ myBlogList[1].date }}</p>
-            <router-link to="/detail">디테일 페이지로</router-link>
+            <router-link :to="{ path: 'detail/' + myBlogList[1].number }"
+                >디테일 페이지로</router-link
+            >
         </div>
 
         <div class="blog-detail">
             <h4>{{ myBlogList[2].title }}</h4>
             <p>{{ myBlogList[2].content }}</p>
             <p>{{ myBlogList[2].date }}</p>
-            <router-link to="/detail">디테일 페이지로</router-link>
+            <router-link :to="{ path: 'detail/' + myBlogList[2].number }"
+                >디테일 페이지로</router-link
+            >
         </div>
     </div>
 </template>
